556341f6bfbf2a4451173214f650d25151460435
sauran <sbaitarakhov@outlook.com>
2025-10-02 22:48:28 +0500

    Refactor Multipart struct to improve field organization and unify channel naming

--- http/request/multipart_channel/multipart_channel.go
+++ http/request/multipart_channel/multipart_channel.go
@@ -33,8 +33,8 @@ const (
 
 type request struct {
 	reqType    requestType
-	key, value string
 	content    io.Reader
+	key, value string
 }
 
 // Multipart provides a streaming multipart/form-data builder for HTTP requests.
@@ -42,12 +42,12 @@ type request struct {
 // All methods (except Send) are chainable for fluent API usage.
 type Multipart struct {
 	client    *http.Client
-	req       *http.Request
-	wg        sync.WaitGroup
+	request   *http.Request
+	requestCh chan request
+	startedCh chan struct{}
 	mw        *multipart.Writer
 	pw        *io.PipeWriter
-	bodyCh    chan request
-	startedCh chan struct{}
+	wg        sync.WaitGroup
 }
 
 // NewMultipart creates a new Multipart builder for streaming multipart/form-data requests.
@@ -57,9 +57,9 @@ func NewMultipart(ctx context.Context, client *http.Client, method, url string) 
 	pr, pw := io.Pipe()
 	started := make(chan struct{})
 
-	m := &Multipart{
+	r := &Multipart{
 		client:    client,
-		bodyCh:    make(chan request, 100),
+		requestCh: make(chan request, 100),
 		pw:        pw,
 		mw:        multipart.NewWriter(pw),
 		startedCh: started,
@@ -67,31 +67,31 @@ func NewMultipart(ctx context.Context, client *http.Client, method, url string) 
 
 	reader := &signalReader{reader: pr, startedCh: started}
 	// Error from NewRequestWithContext is ignored as it only fails for invalid method/URL
-	m.req, _ = http.NewRequestWithContext(ctx, method, url, reader)
-	m.req.Header.Set("Content-Type", m.mw.FormDataContentType())
+	r.request, _ = http.NewRequestWithContext(ctx, method, url, reader)
+	r.request.Header.Set("Content-Type", r.mw.FormDataContentType())
 
-	m.wg.Add(1)
-	go m.worker()
+	r.wg.Add(1)
+	go r.worker()
 
-	return m
+	return r
 }
 
-func (m *Multipart) worker() {
-	defer m.wg.Done()
-	for req := range m.bodyCh {
+func (r *Multipart) worker() {
+	defer r.wg.Done()
+	for req := range r.requestCh {
 		var err error
 		switch req.reqType {
 		case stringType:
-			err = m.mw.WriteField(req.key, req.value)
+			err = r.mw.WriteField(req.key, req.value)
 		case fileType:
 			var part io.Writer
-			part, err = m.mw.CreateFormFile(req.key, req.value)
+			part, err = r.mw.CreateFormFile(req.key, req.value)
 			if err == nil {
 				_, err = io.Copy(part, req.content)
 			}
 		}
 		if err != nil {
-			m.pw.CloseWithError(err)
+			r.pw.CloseWithError(err)
 			return
 		}
 	}
@@ -99,51 +99,51 @@ func (m *Multipart) worker() {
 
 // Param adds a string field to the multipart form.
 // Returns the Multipart instance for method chaining.
-func (m *Multipart) Param(key, value string) *Multipart {
-	m.bodyCh <- request{reqType: stringType, key: key, value: value}
-	return m
+func (r *Multipart) Param(key, value string) *Multipart {
+	r.requestCh <- request{reqType: stringType, key: key, value: value}
+	return r
 }
 
 // Bool adds a boolean field to the multipart form.
 // The value is converted to "true" or "false" string.
-func (m *Multipart) Bool(key string, value bool) *Multipart {
-	return m.Param(key, strconv.FormatBool(value))
+func (r *Multipart) Bool(key string, value bool) *Multipart {
+	return r.Param(key, strconv.FormatBool(value))
 }
 
 // Float adds a float64 field to the multipart form.
 // The value is formatted using strconv.FormatFloat with 'f' format.
-func (m *Multipart) Float(key string, value float64) *Multipart {
-	return m.Param(key, strconv.FormatFloat(value, 'f', -1, 64))
+func (r *Multipart) Float(key string, value float64) *Multipart {
+	return r.Param(key, strconv.FormatFloat(value, 'f', -1, 64))
 }
 
 // Int adds an integer field to the multipart form.
-func (m *Multipart) Int(key string, value int) *Multipart {
-	return m.Param(key, strconv.Itoa(value))
+func (r *Multipart) Int(key string, value int) *Multipart {
+	return r.Param(key, strconv.Itoa(value))
 }
 
 // File adds a file field to the multipart form.
 // The content is read from the provided io.Reader and streamed to the request.
-func (m *Multipart) File(key, filename string, content io.Reader) *Multipart {
-	m.bodyCh <- request{reqType: fileType, key: key, value: filename, content: content}
-	return m
+func (r *Multipart) File(key, filename string, content io.Reader) *Multipart {
+	r.requestCh <- request{reqType: fileType, key: key, value: filename, content: content}
+	return r
 }
 
 // Header sets an HTTP header on the request.
 // This should be called before Send to ensure headers are included in the request.
-func (m *Multipart) Header(key, value string) *Multipart {
-	m.req.Header.Set(key, value)
-	return m
+func (r *Multipart) Header(key, value string) *Multipart {
+	r.request.Header.Set(key, value)
+	return r
 }
 
 // Send executes the HTTP request with all accumulated form data and headers.
 // It waits for the HTTP client to start reading, then closes the form writer
 // and waits for the response. Returns the HTTP response or an error.
-func (m *Multipart) Send() (*http.Response, error) {
+func (r *Multipart) Send() (*http.Response, error) {
 	respCh := make(chan *http.Response, 1)
 	errCh := make(chan error, 1)
 
 	go func() {
-		resp, err := m.client.Do(m.req)
+		resp, err := r.client.Do(r.request)
 		if err != nil {
 			errCh <- err
 			return
@@ -151,12 +151,12 @@ func (m *Multipart) Send() (*http.Response, error) {
 		respCh <- resp
 	}()
 
-	<-m.startedCh
+	<-r.startedCh
 
-	close(m.bodyCh)
-	m.wg.Wait()
-	m.mw.Close()
-	m.pw.Close()
+	close(r.requestCh)
+	r.wg.Wait()
+	r.mw.Close()
+	r.pw.Close()
 
 	select {
 	case resp := <-respCh:
